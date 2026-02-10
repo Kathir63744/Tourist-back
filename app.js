fe9bf64f17324519dc7a1f9fc532b28a1c249726
Kathir63744 <textme.kathir@gmail.com>
2026-02-10 14:55:05 +0530

    add2

--- app.js
+++ app.js
@@ -82,3 +82,17 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    error: 'Internal server error'
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
